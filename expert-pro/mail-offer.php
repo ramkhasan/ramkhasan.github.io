84a8f36ce862480b4b22edc036245b7ba9b861ad
Ramazan <30290308+ramkhasan@users.noreply.github.com>
2022-02-08 06:05:59 +0300

    Add files via upload

--- expert-pro/mail-offer.php
+++ expert-pro/mail-offer.php
@@ -13,12 +13,12 @@ $email = $_POST['offer_form_email'];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'peoplestories@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '$PsdBMol'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'newfrontendtrends@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'GmVBN$3B'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('peoplestories@mail.ru'); // от кого будет уходить письмо?
+$mail->setFrom('newfrontendtrends@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('ramhas87@gmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
