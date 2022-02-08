38ca18c547332d2421237d1bdcb784deb4ee8bf9
Ramazan <30290308+ramkhasan@users.noreply.github.com>
2022-02-08 04:17:48 +0300

    Добавил модальные окна с формой

--- expert-pro/js/main.js
+++ expert-pro/js/main.js
@@ -15,3 +15,26 @@ $("a.target-burger").click(function (){
 //     $("body").removeClass("modal-open");
 //   }
 // });
+
+//Fancybox 
+Fancybox.bind('[data-fancybox="dialog"]', {
+  Carousel: {
+    Panzoom: {
+      touch: false,
+    },
+    Navigation: false,
+  },
+  l10n: {
+    CLOSE: "Закрыть",
+    NEXT: "Следующий",
+    PREV: "Предыдущий",
+    ERROR: "Запрошенный контент не загружается. <br/> Пожалуйста, повторите попытку позже.",
+    PLAY_START: "Начать слайд-шоу",
+    PLAY_STOP: "Приостановить слайд-шоу",
+    FULL_SCREEN: "На полный экран",
+    THUMBS: "Миниатюры",
+    DOWNLOAD: "Скачать",
+    SHARE: "Поделиться",
+    ZOOM: "Увеличить"
+  },
+});
