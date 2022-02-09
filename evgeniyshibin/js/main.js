cc75a2c12bcdc1f0829982c8dbd94e127ee2cf61
Ramazan <30290308+ramkhasan@users.noreply.github.com>
2022-02-09 20:38:56 +0300

    Внёс правки

--- evgeniyshibin/js/main.js
+++ evgeniyshibin/js/main.js
@@ -18,55 +18,18 @@ $(window).scroll(function () {
       }
     };
   });
-})
-.scroll();
-
-// Добавление ссылки при прокрутке на верх страницы
-$(window).scroll(function() {
-  let height = $(window).scrollTop();
-  if(height > 400){
-    document.getElementById("projects").href="#";
-  } else{
-    document.getElementById("projects").href="/evgeniyshibin/projects.html";
-  }
 });
-// $(window).scroll(function() {
-//   let height = $(window).scrollTop();
-//   if(height > 400){
-//     document.getElementById("about").href="#";
-//   } else{
-//     document.getElementById("about").href="/evgeniyshibin/about.html";
-//   }
-// });
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    document.getElementById("about").href="#";
-  } else {
-    document.getElementById("about").href="/evgeniyshibin/about.html";
-  }
-}
 
 // Плавная прокрутка на верх страницы
-$("#header-logo").click(function() {
+$("#header-logo").click(function () {
   $("html, body").animate({scrollTop: 0}, 1500);
-  return false;
-});
-$("#projects").click(function() {
-  $("html, body").animate({scrollTop: 0}, 1500);
-  return false;
-});
-$("#about").click(function() {
-  $("html, body").animate({scrollTop: 0}, 1500);
-  return false;
 });
 
 // Menu
-$("a.target-burger").click(function (){
+$("a.target-burger").click(function () {
   $("nav.main-nav, a.target-burger").toggleClass("toggled");
 });
-$(document).mouseup(function (e){  
+$(document).mouseup(function (e) {  
   let div = $("nav.main-nav, a.target-burger");
   if (!div.is(e.target) && div.has(e.target).length === 0) {  
     div.removeClass("toggled");
